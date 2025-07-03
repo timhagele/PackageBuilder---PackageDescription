c6d3fd142d3706dbbd423858f96a0ef53feb726d
Timothy Hagele <timhagele@icloud.com>
2025-07-03 02:22:39 -0400

    Initial Commit

---
+++ PackageBuilder 1.3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PackageBuilder 1.3
+//
+//  Created by Tim Hagele on 7/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PackageBuilder 1.3/PackageBuilder_1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  PackageBuilder_1_3App.swift
+//  PackageBuilder 1.3
+//
+//  Created by Tim Hagele on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PackageBuilder_1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
